a88f5974a9b801e65d186feddf5deba6d2ef3a46
Massiel Diaz <massieldiaz.c@gmail.com>
2025-05-22 01:14:44 -0400

    first commit

---
+++ citas.js
@@ -0,0 +1,71 @@
+
+let citas = [
+    {
+        autor: 'Goku',
+        texto: 'El poder viene en respuesta a una necesidad, no a un deseo.'
+    },
+    {
+        autor: 'Kakashi Hatake',
+        texto: 'En el mundo ninja, aquellos que rompen las reglas son escoria... pero aquellos que abandonan a sus amigos son peor que escoria.'
+    },
+    {
+        autor: 'L (Death Note)',
+        texto: 'No puedes ganar si siempre estás tratando de evitar perder.'
+    },
+    {
+        autor: 'Levi Ackerman',
+        texto: 'La única cosa que somos capaces de hacer es creer que no nos arrepentiremos de la elección que hicimos.'
+    },
+    {
+        autor: 'Naruto Uzumaki',
+        texto: 'Nunca retrocedo a mi palabra... ese es mi nindō: mi camino ninja.'
+    },
+    {
+        autor: 'Roronoa Zoro',
+        texto: '¡Entrenaré más duro para no volver a perder nunca más!'
+    },
+    {
+        autor: 'Kenshin Himura',
+        texto: 'No importa lo fuerte que seas, nunca uses la fuerza para herir a otros.'
+    },
+    {
+        autor: 'Edward Elric',
+        texto: 'Una lección sin dolor no tiene sentido. Eso es porque no se puede ganar sin sacrificio.'
+    },
+    {
+        autor: 'Itachi Uchiha',
+        texto: 'Las personas viven sus vidas atadas a lo que aceptan como correcto y verdadero. Eso es lo que los llama realidad.'
+    },
+    {
+        autor: 'Light Yagami',
+        texto: 'Ser un dios en el nuevo mundo es el objetivo que me he propuesto.'
+    },
+    {
+        autor: 'Makunouchi Ippo',
+        texto: 'No me importa si me golpean. Solo quiero tener el coraje de avanzar.'
+    },
+    {
+        autor: 'Yusuke Urameshi',
+        texto: 'Ser fuerte no es solo una cuestión de poder físico. Se trata de tener el coraje de seguir adelante.'
+    },
+    {
+        autor: 'Kamado Tanjiro',
+        texto: 'Respira. Calma tu mente. Nunca pierdas la compostura.'
+    },
+    {
+        autor: 'Jiraiya',
+        texto: 'Un verdadero ninja es aquel que soporta el dolor y sigue avanzando.'
+    },
+    {
+        autor: 'Nanami Kento',
+        texto: 'No es que quiera morir. Solo que no quiero trabajar más allá de las 6.'
+    },
+    {
+        autor: 'Killua Zoldyck',
+        texto: 'La gente cambia cuando se da cuenta de lo que está en juego.'
+    },
+    {
+        autor: 'Shanks',
+        texto: 'No me importa si pierdo un brazo, siempre y cuando mis amigos estén a salvo.'
+    }
+];
